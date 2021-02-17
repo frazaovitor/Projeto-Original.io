62cb229d3201340208ac6cc516c0e4586277718a
frazaovitor <51264703+frazaovitor@users.noreply.github.com>
2021-02-17 15:28:56 -0300

    carousel e footer

--- script/script.js
+++ script/script.js
@@ -1,21 +1,44 @@
-$(document).ready(function(){
+// Carousel destaque
 
-    $('.carousel').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear',
-        vertical: true,
-        verticalSwiping: true
-      });
+$(document).ready(function () {
+  $('.carouselG').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.carouselP'
   });
 
+  $('.carouselP').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.carouselG',
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true,
+    vertical: true,
+    arrows: true,
+    prevArrow: ".nav-cima",
+    nextArrow: ".nav-baixo"
+  });
+});
+
+// Carousel também vistos
+
+$(document).ready(function () {
+  $('.carouselOfertas').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: ".oferta-esq",
+    nextArrow: ".oferta-dir"
+  });
+});
 
 
 
 
-  
 
 
 
